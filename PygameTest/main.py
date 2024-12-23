c0a16ce136bb1c1d0a48b9802f592f04c921b298
sunkgrub <sunkgrub@gmail.com>
2024-12-23 18:31:30 -0500

    Add game loop and event handling to main.py

--- PygameTest/main.py
+++ PygameTest/main.py
@@ -1,11 +1,20 @@
 import pygame
+from sys import exit
 
 # Initialize the game
 pygame.init()
 
 # Set up the screen
 screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
     # draw all our elements
+    # update everything
     pygame.display.update()
+    clock.tick(60)
