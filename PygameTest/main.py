9fdd0f98c8bef2b0c07a76f0b0c0587e94e81bd4
sunkgrub <sunkgrub@gmail.com>
2024-12-23 19:01:53 -0500

    Add player and obstacle classes with basic functionality

--- PygameTest/main.py
+++ PygameTest/main.py
@@ -9,12 +9,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill('red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update everything
+   
+    screen.blit(test_surface, (200, 100))
+
     pygame.display.update()
     clock.tick(60)
