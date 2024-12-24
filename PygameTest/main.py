34a9f1996d93efade7ccab7518e69e81fbe49c9b
sunkgrub <sunkgrub@gmail.com>
2024-12-23 19:54:37 -0500

    Add background and text rendering to main.py

--- PygameTest/main.py
+++ PygameTest/main.py
@@ -8,9 +8,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('red')
+sky_surface = pygame.image.load('graphics/sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'black',)
 
 while True:
     for event in pygame.event.get():
@@ -18,7 +20,9 @@ while True:
             pygame.quit()
             exit()
    
-    screen.blit(test_surface, (200, 100))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300,50))
 
     pygame.display.update()
     clock.tick(60)
