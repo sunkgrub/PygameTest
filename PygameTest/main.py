fd4777c6aa0e397d3153522fd4d1a99ae89eeda2
sunkgrub <sunkgrub@gmail.com>
2024-12-30 17:33:53 -0500

    Updated Main.py

--- PygameTest/main.py
+++ PygameTest/main.py
@@ -10,19 +10,38 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load('graphics/sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+
 text_surface = test_font.render('My game', False, 'black',)
+text_rect = text_surface.get_rect(center=(400,50))
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision!')
    
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300,50))
+    screen.blit(text_surface, text_rect)
+
+    snail_rect.x -= 4
+    if snail_rect.x <= -100:
+        snail_rect.x = 800
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
